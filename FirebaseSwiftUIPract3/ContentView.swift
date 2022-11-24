597d2765349bad6f2a2e6cf2ed29b89526516aa5
shivya-agg <shivya.aggarwal@capgemini.com>
2022-11-23 23:29:49 -0800

    Initial Commit

---
+++ FirebaseSwiftUIPract3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirebaseSwiftUIPract3
+//
+//  Created by Capgemini-DA202 on 11/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirebaseSwiftUIPract3/FirebaseSwiftUIPract3App.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseSwiftUIPract3App.swift
+//  FirebaseSwiftUIPract3
+//
+//  Created by Capgemini-DA202 on 11/23/22.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseSwiftUIPract3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
